e6311886357ca9808f8884937893823020c7f04f
Ever <evermedina2025@gmail.com>
2021-10-29 13:42:46 -0700

    fetch api

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-// var apiKey = "945207e9ffc2bc2d91952c86c2a2bba4";
-// var strLon;
-// var strLat;
+var apiKey = "945207e9ffc2bc2d91952c86c2a2bba4";
+var currentCity="";
+var lastCity="";
+
+var handleErrors = (response) => {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
+// Display current conditions
+var getCurrentConditions = (event) => {
+
+  let city=$('#search-city').val();
+  currentCity=$('#search-city').val();
+
+  // fetch from weather API
+  let queryUrl="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&&appid" + apiKey;
+}
+
 // var weather = document.querySelector(".current");
 // var humidity = document.querySelector("fiveDay");
 // function getWeather(latitude,longitude){
@@ -60,91 +78,3 @@
 //   pHumid.textContent = object.current.humidity
 //   document.querySelector("fiveDay").appendChild(pHumid);
 // }
-
-var apiKey="945207e9ffc2bc2d91952c86c2a2bba4";
-
-function currentWeather(){
-  navigator.geolocation.getCurrentPosition(function(position) {
-    longitude = position.coords.longitude;
-    latitude = position.coords.latitude;
-
-    var queryURL ="http://api.openweathermap.org/geo/1.0/direct?q=' + location + '&limit=5&appid='+ apiKey";
-
-    $.ajax ({
-      url: queryURL,
-      method:"GET"
-    })
-    .then(function(response) {
-     var icons = response.weather[0].icon;
-
-    var urlIcon="http://openweathermap.org/img/w/" + icons + ".png";
-    $(".city").html("<h1>" + response.name + "</h1>");
-    $(".temp").text("temperature:" + ((response.main.temp - 273.15) * 1.8 +32).toFixed(0)+ "°F");
-    $(".humidity").text("Humidity:" + response.main.humidity + "%");
-    $(".wind").text("Wind Speed:" + response.wind.speed + "MPH");
-    $("#wicon").attr("src", urlIcon);
-
-  });
-});
-};
-
-currentWeather();
-
-function fiveDayForecast(){
-
-  var fiveDayLink = "https://api.openweathermap.org/data/2.5/forecast?q=fontana&appid=" + apiKey;
-
-  $.ajax({
-    url: fiveDayLink,
-    method:"GET"
-  }).then(function(resopnseTwo){
-
-    var day1 = responseTwo.list[4].dt_txt;
-    
-    var day2 = responseTwo.list[12].dt_txt;
-    
-    var day3 = responseTwo.list[20].dt_txt;
-    
-    var day4 = responseTwo.list[28].dt_txt;
-    
-    var day5 = responseTwo.list[36].dt_txt;
-
-  $("#day1").html("<h4>" + day1.substr(0,10) + "</h4>")
-  $("#day1").html("<h4>" + day1.substr(0,10) + "</h4>")
-  $("#day1").html("<h4>" + day1.substr(0,10) + "</h4>")
-  $("#day1").html("<h4>" + day1.substr(0,10) + "</h4>")
-
-  $("#day2").html("<h4>" + day1.substr(0,10) + "</h4>")
-  $("#day2").html("<h4>" + day1.substr(0,10) + "</h4>")
-  $("#day2").html("<h4>" + day1.substr(0,10) + "</h4>")
-  $("#day2").html("<h4>" + day1.substr(0,10) + "</h4>")
-
-  $("#day3").html("<h4>" + day1.substr(0,10) + "</h4>")
-  $("#day3").html("<h4>" + day1.substr(0,10) + "</h4>")
-  $("#day3").html("<h4>" + day1.substr(0,10) + "</h4>")
-  $("#day3").html("<h4>" + day1.substr(0,10) + "</h4>")
-
-  $("#day4").html("<h4>" + day1.substr(0,10) + "</h4>")
-  $("#day4").html("<h4>" + day1.substr(0,10) + "</h4>")
-  $("#day4").html("<h4>" + day1.substr(0,10) + "</h4>")
-  $("#day4").html("<h4>" + day1.substr(0,10) + "</h4>")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  })
-
-
-
-
-}
