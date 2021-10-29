c0204fc84dd31566d65705965e02bfdddfc66432
Ever <evermedina2025@gmail.com>
2021-10-29 13:27:47 -0700

    edited html page

--- script.js
+++ script.js
@@ -89,3 +89,62 @@ function currentWeather(){
 };
 
 currentWeather();
+
+function fiveDayForecast(){
+
+  var fiveDayLink = "https://api.openweathermap.org/data/2.5/forecast?q=fontana&appid=" + apiKey;
+
+  $.ajax({
+    url: fiveDayLink,
+    method:"GET"
+  }).then(function(resopnseTwo){
+
+    var day1 = responseTwo.list[4].dt_txt;
+    
+    var day2 = responseTwo.list[12].dt_txt;
+    
+    var day3 = responseTwo.list[20].dt_txt;
+    
+    var day4 = responseTwo.list[28].dt_txt;
+    
+    var day5 = responseTwo.list[36].dt_txt;
+
+  $("#day1").html("<h4>" + day1.substr(0,10) + "</h4>")
+  $("#day1").html("<h4>" + day1.substr(0,10) + "</h4>")
+  $("#day1").html("<h4>" + day1.substr(0,10) + "</h4>")
+  $("#day1").html("<h4>" + day1.substr(0,10) + "</h4>")
+
+  $("#day2").html("<h4>" + day1.substr(0,10) + "</h4>")
+  $("#day2").html("<h4>" + day1.substr(0,10) + "</h4>")
+  $("#day2").html("<h4>" + day1.substr(0,10) + "</h4>")
+  $("#day2").html("<h4>" + day1.substr(0,10) + "</h4>")
+
+  $("#day3").html("<h4>" + day1.substr(0,10) + "</h4>")
+  $("#day3").html("<h4>" + day1.substr(0,10) + "</h4>")
+  $("#day3").html("<h4>" + day1.substr(0,10) + "</h4>")
+  $("#day3").html("<h4>" + day1.substr(0,10) + "</h4>")
+
+  $("#day4").html("<h4>" + day1.substr(0,10) + "</h4>")
+  $("#day4").html("<h4>" + day1.substr(0,10) + "</h4>")
+  $("#day4").html("<h4>" + day1.substr(0,10) + "</h4>")
+  $("#day4").html("<h4>" + day1.substr(0,10) + "</h4>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  })
+
+
+
+
+}
