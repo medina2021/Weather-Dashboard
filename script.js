63dc8f89471adea7550074047a521fa15ca5a0fc
Ever <evermedina2025@gmail.com>
2021-10-29 14:20:57 -0700

    Lat & long and uv index

--- script.js
+++ script.js
@@ -17,6 +17,64 @@ var getCurrentConditions = (event) => {
 
   // fetch from weather API
   let queryUrl="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&&appid" + apiKey;
+  fetch(queryUrl)
+  .then(handleErrors)
+  .then((response)=>{
+    return response.json();
+  })
+
+  .then((response)=> {
+      // save to local storage
+      saveCity(city);
+      $('#search-error').text("");
+
+      let currentWeatherIcon="https://openweathermap.org/img/w/" +response.weather[0].icon + ".png";
+      
+      let currentTimeUTC = response.dt;
+      let currentTimeZoneOffset = response.timezone;
+      let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60/ 60;
+      let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
+      
+      // Render the cities
+      renderCities();
+
+      getFiveDayForecast(event);
+      $('#header-text').text(response.name);
+      // Search Results
+      let currentWeatherHTML=`
+      <h3>${response.name} ${currentMoment.format("(MM/DD/YY)")}<img src="${currentWeatherIcon}"></h3>
+      <ul class="list-unstyled">
+      <li>Temperature: ${response.main.temp}&#8457;</li>
+      <li>Humidity: ${response.main.humidity}%</li>
+      <li>Wind Speed: ${response.wind.speed} mph</li>
+      <li id="uvindex"> UV Index:</li>
+      </ul>`;
+      // append results
+      $('#current-weather').html(currentWeatherHTML);
+
+      let latitude = response.coord.lat;
+      let longitude = response.coord.lon;
+      let uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude+ "&units=imperial&&appid="+ apiKey;
+
+      uvQueryURL ="https://cors-anywhere.herokuapp.com/" + uvQueryURL;
+
+      fetch(uvQueryURL)
+      .then(handleErrors)
+      .then((response) =>{
+        return response.json();
+      })
+      .then((response) =>{
+        let uvIndex = response.value;
+        $('#uvIndex').html(`UV Index: <span id="uvVal"> ${uvIndex}</span>`);
+        if (uvIndex >= 0 && uvIndex<3){
+          $('#uvVal').attr("class", "uv-favorable");
+        } else if (uvIndex>=3 && uvIndex<8){
+          $('#uvVal').attr("class", "uv-moderate");
+        } else if (uvIndex>=8){
+          $('#uvVal').attr("class", "uv-severe");
+        }
+      });
+  })
 }
 
 // var weather = document.querySelector(".current");
